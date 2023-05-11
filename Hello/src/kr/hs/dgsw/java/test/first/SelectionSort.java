ecfcfc5a471bca5a0b208ac2cc4d180d2fea92dd
stev3j <mygajeong@gmail.com>
2023-05-11 10:58:26 +0900

    ✨ :: Test
    담주 수욜

---
+++ Hello/src/kr/hs/dgsw/java/test/first/SelectionSort.java
@@ -0,0 +1,17 @@
+package kr.hs.dgsw.java.test.first;
+
+public class SelectionSort {
+
+    public int[] sort(int[] unsorted) {
+        return this.doSelectionSorting(unsorted);
+    }
+
+
+    private int[] doSelectionSorting(int[] unsorted) {
+        // TODO : 선택 정열을 통해서 unsorted 오름차순으로 정렬해서 반환하세요.
+
+        return unsorted;
+    }
+
+
+}
